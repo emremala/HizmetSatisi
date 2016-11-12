81b1bddbb92dde3e5200e74aadfebac718b73c93
YAĞIZ <YAIZ@DESKTOP-CNU8LVE>
2016-11-13 00:29:53 +0200

    Add project files.

---
+++ HizmetSatisi/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HizmetSatisi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ HizmetSatisi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HizmetSatisi.MvcApplication" Language="C#" %>
